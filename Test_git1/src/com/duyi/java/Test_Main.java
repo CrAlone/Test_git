741608f3f6f10d84961337981a95fa9bcfdf492a
${CrAlone} <${Cr__Alone@163.com}>
2020-04-20 17:53:06 +0800

    test分支3  增加hello test

--- Test_git1/src/com/duyi/java/Test_Main.java
+++ Test_git1/src/com/duyi/java/Test_Main.java
@@ -3,5 +3,6 @@ package com.duyi.java;
 public class Test_Main {
     public static void main(String[] args) {
         System.out.println("hole master");
+        System.out.println("hello test");
     }
 }
